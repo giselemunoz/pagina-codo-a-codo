4f0fa29e1503e8699e553638c8ad5a0bb4f46481
GiseleMuñoz <84409210+giselemunoz@users.noreply.github.com>
2022-11-04 20:11:30 -0300

    pagina js
    se creo una pagina con las funciones de JavaScript

---
+++ script.js
@@ -0,0 +1,30 @@
+function calcularTotal() {
+    valor = document.getElementById('cantInput').value;
+    valor = parseInt(valor);
+    total = valor * 200;
+    porcentaje = 0;
+    categoria = document.getElementById('categ').value;
+    if (categoria == 1) {
+        porcentaje = total * 80 / 100;
+        total = total - porcentaje;
+    }
+
+
+    if (categoria == 2) {
+        porcentaje = total * 50 / 100;
+        total = total - porcentaje;
+    }
+
+
+    if (categoria == 3) {
+        porcentaje = total * 15 / 100;
+        total = total - porcentaje;
+    }
+
+
+    document.getElementById('cantTotal').innerHTML = "Total a pagar : " + " $" + total;
+}
+function borrar() {
+    document.getElementById('myForm').reset();
+    document.getElementById('cantTotal').innerHTML = "Total a Pagar: $";
+}
